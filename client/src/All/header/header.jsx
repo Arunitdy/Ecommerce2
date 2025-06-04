46b291358568cd458eea2840639c144f947d3ea7
Arun <arunmundakkal003@gmail.com>
2025-06-04 20:08:01 +0530

    chore: remove unused react.svg asset

---
+++ client/src/All/header/header.jsx
@@ -0,0 +1,12 @@
+
+
+export const header = () => {
+
+
+  return (
+    <div className="header">
+      <h1>Flatlogic</h1>
+      <p>Your one-stop solution for all your needs.</p>
+    </div>
+  );
+}
